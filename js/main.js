a68913aa0e4ea7806040e707301d74eb59fcf6d5
Michal Muszynski <m.muszy@gmail.com>
2018-08-15 14:08:40 +0200

    Slide social icons

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const navigationList = document.querySelector('.navigation-list');
 const socialIcon = document.querySelector('.social');
-const socialIconsList = document.querySelector('.social-list-item');
 const hamburgerBars = Array.from(document.querySelectorAll('.bar'));
-
-socialIcon.addEventListener('click', function() {
-  if (socialIconsList.classList.contains('show')) {
-    socialIconsList.style.display = 'none';
-    socialIconsList.classList.remove('show');
-  } else  {
-    socialIconsList.style.display = 'inline-flex';
-    socialIconsList.classList.add('show');
-  }
-});
 
 hamburgerIcon.addEventListener('click', function() {
   hamburgerBars.forEach(bar => bar.classList.toggle('active'));
   navigationList.classList.toggle('closed')
 });
 
+
+socialIcon.addEventListener('click', function() {
+  navigationList.classList.toggle('shift')
+});
 
 
 
