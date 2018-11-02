3d269293af2abc93a86127db9adec68de9de0237
Michal Muszynski <m.muszy@gmail.com>
2018-11-02 23:25:22 +0100

    Optimize css

--- js/main.js
+++ js/main.js
@@ -3,19 +3,11 @@ const navigationList = document.querySelector('.navigation-list');
 const socialIcon = document.querySelector('.social');
 const hamburgerBars = Array.from(document.querySelectorAll('.bar'));
 
-const compression = require('compression');
-const app = express();
-app.use(compression());
-
 hamburgerIcon.addEventListener('click', function() {
   hamburgerBars.forEach(bar => bar.classList.toggle('active'));
   navigationList.classList.toggle('closed')
 });
 
-
 socialIcon.addEventListener('click', function() {
   navigationList.classList.toggle('shift')
 });
-
-
-
