f413252a3b5de7a47d18414cd706f5aa4ad0435e
Michal Muszynski <m.muszy@gmail.com>
2018-11-02 22:04:59 +0100

    Add express compression

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const navigationList = document.querySelector('.navigation-list');
 const socialIcon = document.querySelector('.social');
 const hamburgerBars = Array.from(document.querySelectorAll('.bar'));
 
+const compression = require('compression');
+const app = express();
+app.use(compression());
+
 hamburgerIcon.addEventListener('click', function() {
   hamburgerBars.forEach(bar => bar.classList.toggle('active'));
   navigationList.classList.toggle('closed')
